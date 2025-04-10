9a557bbb2fdea0cf3edb4b70932fbe65eac5b41a
BassaniDavid <davbassan98@gmail.com>
2025-04-10 15:33:24 +0200

    feat:  added movie list in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const dataMovie = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
 
   return (
     <>
-
+      <ul>
+        {dataMovie.map((movie, index) =>
+          <div key={index}>
+            <h2>titolo</h2>
+            <p>{movie.title}</p>
+            <h3>genere</h3>
+            <p>{movie.genre}</p>
+          </div>
+        )}
+      </ul>
     </>
   )
 }
